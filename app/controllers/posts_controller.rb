ee6e4ec98895a37ebc49d302911810c3f6bedb5a
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-18 16:52:57 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @post.update(post_params)
 
-    if @post.valid
+    if @post.valid?
       redirect_to post_path(@post)
     else
       render :edit
